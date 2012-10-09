4bd0133a0f4f8f0682863b37d5eea12ffeecbe44
Matt West <git@matthewwest.co.uk>
2012-10-10 00:03:14 +0200

    Add export list to Base.hs

--- Data/EnumMapMap/Base.hs
+++ Data/EnumMapMap/Base.hs
@@ -20,7 +20,35 @@
 -- in EnumMapMap.Lazy and EnumMapMap.Strict.
 -----------------------------------------------------------------------------
 
-module Data.EnumMapMap.Base where
+module Data.EnumMapMap.Base(
+            -- * Key types
+            (:&)(..), K(..), N(..), Z(..),
+            d1, d2, d3, d4, d5, d6, d7, d8, d9, d10,
+            -- * Split/Join Keys
+            IsSplit,
+            Plus,
+            Head,
+            Tail,
+            splitKey,
+            -- * Internal
+            -- ** IsEMM
+            EMM(..),
+            IsEmm(..),
+            EnumMapMap(..),
+            -- ** EMM internals
+            mergeWithKey',
+            mapWithKey_,
+            foldrWithKey_,
+            foldlStrict,
+            -- ** IntMap internals
+            Key,
+            bin,
+            tip,
+            nomatch,
+            match,
+            join,
+            zero
+) where
 
 import           Prelude hiding (lookup,
                                  map,
